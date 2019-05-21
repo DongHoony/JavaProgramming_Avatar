a44c2ab8313454429701ad319529e9a6f1f57203
DongHoon Lee <31026350+DongHoony@users.noreply.github.com>
2019-05-21 13:46:41 +0900

    Update Zuko.java

--- Zuko.java
+++ Zuko.java
@@ -16,28 +16,15 @@ public class Zuko extends Fire {
         this.skill4();
     }
     public void skill1(){
-        //Fireball
-        skills.setDamage(0, 25);
-        skills.setRequiredEnergy(0, 15);
-        skills.setRange(0, 1,3,4,5,7);
     }
     public void skill2(){
-        //FlameWheel
-        skills.setDamage(1, 40);
-        skills.setRequiredEnergy(1, 30);
-        skills.setRange(1, 3,4,5);
+       
     }
     public void skill3(){
-        //Flame
-        skills.setDamage(2, 35);
-        skills.setRequiredEnergy(2, 25);
-        skills.setRange(2, 0,2,6,8);
+       
     }
     public void skill4(){
-        //FireFist
-        skills.setDamage(3, 75);
-        skills.setRequiredEnergy(3, 100);
-        skills.setRange(3, 4);
+        
     }
     public void skill5(){
         skills.skill5();
