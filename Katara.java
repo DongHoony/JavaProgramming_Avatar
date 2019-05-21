821df1a718d1fdb4d5ca1b7dc834d55e48f9c3a9
DongHoon Lee <31026350+DongHoony@users.noreply.github.com>
2019-05-21 13:46:18 +0900

    Update Katara.java

--- Katara.java
+++ Katara.java
@@ -17,29 +17,16 @@ public class Katara extends Water {
         this.skill4();
     }
     public void skill1(){
-        skills.setDamage(0, 0);
-        skills.setRequiredEnergy(0, 20);
-        skills.setRange(0, 4);
-        this.setHealth(this.getHealth()+20);
-        skills.setSkillName(0, "Skill name");
+       
     }
     public void skill2(){
-        skills.setDamage(1, 35);
-        skills.setRequiredEnergy(1, 35);
-        skills.setRange(1, 2,4,6);
-        skills.setSkillName(1, "Skill name");
+        
     }
     public void skill3(){
-        skills.setDamage(2, 50);
-        skills.setRequiredEnergy(2, 35);
-        skills.setRange(2, 1,3,4,5,7);
-        skills.setSkillName(2, "Skill name");
+        
     }
     public void skill4(){
-        skills.setDamage(3, 30);
-        skills.setRequiredEnergy(3, 30);
-        skills.setRange(3, 0,2,4,6,8);
-        skills.setSkillName(3, "Skill name");
+        
     }
     public void skill5(){
         skills.skill5();
